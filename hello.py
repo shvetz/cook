1685e9ef03f622a6de94b0567bd22ee699fa56b9
Vitalii Shvetz <shvetsvo@ya.ru>
2012-12-18 14:17:21 +0200

    First render template

--- hello.py
+++ hello.py
@@ -25,5 +25,10 @@ def projects():
 def about():
     return 'The about page'
 
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
 if __name__ == '__main__':
     app.run()
