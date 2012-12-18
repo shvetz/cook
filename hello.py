8ddc745b7e9af0215b9fa5864e96175b17a2aeea
Vitalii Shvetz <shvetsvo@ya.ru>
2012-12-18 11:58:17 +0200

    First try in Flask

---
+++ hello.py
@@ -0,0 +1,29 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+@app.route('/user')
+def user():
+    return 'This is user page'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'Im a %s' %username
+
+@app.route('/post/<int:post_id>')
+def post (post_id):
+    return 'This is post number %d' %post_id
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route ('/about')
+def about():
+    return 'The about page'
+
+if __name__ == '__main__':
+    app.run()
